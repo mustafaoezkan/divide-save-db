8ab439f4de51f3a40a6b6a75f90e814c2e90b60f
Mustafa Özkan <me@mustafaoezkan.com.tr>
2024-11-16 23:33:19 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -20,6 +20,14 @@ CREATE TABLE tokens (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE refresh_tokens (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    token VARCHAR(500) NOT NULL,
+    expiry_date TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 CREATE TABLE videos (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
